c7fa433179c86f8caada4a1910b7667356b1f2fe
Fercasanov <fernando.casanova1992@outlook.com>
2023-08-25 12:49:34 -0300

    se resolvio el problema del calculo delr esultado

--- app.py
+++ app.py
@@ -10,23 +10,26 @@ def index():
     return render_template("quizz-game/index.html")
 
 
-@app.route("/cuestionario/<int:id>'")
+@app.route("/cuestionario/<int:id>")
 def cuestionario(id):
     dict = data_Cuestionarios(id)
     return render_template("quizz-game/cuestionario.html", pregunta=dict, id=id)
 
 
-@app.route("/cuestionario/resultado", methods=['POST'])
-def resultado():
-    options_choices = request.form
+@app.route("/cuestionario/<int:id>/resultado", methods=['POST'])
+def resultado(id):
+ 
+    options_choices =list(dict(request.form).values())  
     print(options_choices)
+    option_correct = answer_Correct(id)
+    print(option_correct)
     resultado = 0
 
-    # for i in range(len(option_value)):
+    for i in range(len(options_choices)):
 
-    #     if int(option_value[i]) == option_correct[i]:
+            if int(options_choices[i]) == option_correct[i]:
 
-    #         resultado +=1
+             resultado +=1
             
     return render_template("quizz-game/resultado.html", resultado = resultado)
 
