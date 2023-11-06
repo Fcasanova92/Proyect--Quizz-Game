ff90b33ca24b3f3607278829e63262e54beff5fc
Fercasanov <fernando.casanova1992@outlook.com>
2023-11-06 20:33:18 -0300

    creacion de archivo data

--- model.py
+++ model.py
@@ -1,34 +1,8 @@
+from data import cuestionarios
 
 def data_Cuestionarios(num_cuestionario):
 
-    cuestionarios = (({"numero":1, "titulo": "¿Cuál país es africano?" , "respuestas":["Peru", "Chile", "Nigeria"] }  ,
-               
-               {"numero":2, "titulo": "¿Cuál país es europeo?" , "respuestas":["Peru", "Escocia", "Mozambique"] },
-                
-            {"numero":3, "titulo": "¿Cuál país es americano?" , "respuestas":["Peru", "Escocia", "Mozambique"] }, 
-                ) ,
-                
-                ({"numero":1, "titulo": "¿Cuál país es africano?" , "respuestas":["Peru", "Chile", "Nigeria"] }  ,
-               
-               {"numero":2, "titulo": "¿Cuál país es europeo?" , "respuestas":["Peru", "Escocia", "Mozambique"] },
-                
-            {"numero":3, "titulo": "¿Cuál país es americano?" , "respuestas":["Peru", "Escocia", "Mozambique"] }, 
-                ),
-                
-                ({"numero":1, "titulo": "¿Cuál país es africano?" , "respuestas":["Peru", "Chile", "Nigeria"] }  ,
-               
-               {"numero":2, "titulo": "¿Cuál país es europeo?" , "respuestas":["Peru", "Escocia", "Mozambique"] },
-                
-            {"numero":3, "titulo": "¿Cuál país es americano?" , "respuestas":["Peru", "Escocia", "Mozambique"] }, 
-                ) ,
-                
-                ({"numero":1, "titulo": "¿Cuál país es africano?" , "respuestas":["Peru", "Chile", "Nigeria"] }  ,
-               
-               {"numero":2, "titulo": "¿Cuál país es europeo?" , "respuestas":["Peru", "Escocia", "Mozambique"] },
-                
-            {"numero":3, "titulo": "¿Cuál país es americano?" , "respuestas":["Peru", "Escocia", "Mozambique"] }, 
-                ))
-
+   
     return cuestionarios[num_cuestionario]
 
     
