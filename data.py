dc9630b330b133421f75ccea4c1168ec67a82e4c
Fercasanov <fernando.casanova1992@outlook.com>
2023-11-08 21:08:29 -0300

    creacion archivo data

---
+++ data.py
@@ -0,0 +1,27 @@
+cuestionarios = (({"numero":1, "titulo": "¿Cuál país es africano?" , "respuestas":["Peru", "Chile", "Nigeria"] }  ,
+               
+               {"numero":2, "titulo": "¿Cuál país es europeo?" , "respuestas":["Peru", "Escocia", "Mozambique"] },
+                
+            {"numero":3, "titulo": "¿Cuál país es americano?" , "respuestas":["Peru", "Escocia", "Mozambique"] }, 
+                ) ,
+                
+                ({"numero":1, "titulo": "¿Cuál país es africano?" , "respuestas":["Peru", "Chile", "Nigeria"] }  ,
+               
+               {"numero":2, "titulo": "¿Cuál país es europeo?" , "respuestas":["Peru", "Escocia", "Mozambique"] },
+                
+            {"numero":3, "titulo": "¿Cuál país es americano?" , "respuestas":["Peru", "Escocia", "Mozambique"] }, 
+                ),
+                
+                ({"numero":1, "titulo": "¿Cuál país es africano?" , "respuestas":["Peru", "Chile", "Nigeria"] }  ,
+               
+               {"numero":2, "titulo": "¿Cuál país es europeo?" , "respuestas":["Peru", "Escocia", "Mozambique"] },
+                
+            {"numero":3, "titulo": "¿Cuál país es americano?" , "respuestas":["Peru", "Escocia", "Mozambique"] }, 
+                ) ,
+                
+                ({"numero":1, "titulo": "¿Cuál país es africano?" , "respuestas":["Peru", "Chile", "Nigeria"] }  ,
+               
+               {"numero":2, "titulo": "¿Cuál país es europeo?" , "respuestas":["Peru", "Escocia", "Mozambique"] },
+                
+            {"numero":3, "titulo": "¿Cuál país es americano?" , "respuestas":["Peru", "Escocia", "Mozambique"] }, 
+                ))
